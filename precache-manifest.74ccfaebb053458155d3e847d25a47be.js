cbd93ae37845a2398be45e689096a691325af4f4
arstkvs <arstkvs@gmail.com>
2019-07-26 11:02:31 +0300

    Updates

---
+++ precache-manifest.74ccfaebb053458155d3e847d25a47be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7526058979ef2b89925be34d07c25d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7374b88848da0854b488",
+    "url": "/static/css/main.f36eb11f.chunk.css"
+  },
+  {
+    "revision": "5fa0921c557f536eb9fc",
+    "url": "/static/js/2.a839423b.chunk.js"
+  },
+  {
+    "revision": "7374b88848da0854b488",
+    "url": "/static/js/main.5f9baa1a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

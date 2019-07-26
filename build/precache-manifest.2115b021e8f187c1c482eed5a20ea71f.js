453278e4deec2073a9df5e0c9a3200781d0b3646
arstkvs <arstkvs@gmail.com>
2019-07-26 11:10:32 +0300

    gh- pages

---
+++ build/precache-manifest.2115b021e8f187c1c482eed5a20ea71f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0631fef645cbf6b1aff6546d8ff7542f",
+    "url": "/cat/index.html"
+  },
+  {
+    "revision": "594d1db979852292f46e",
+    "url": "/cat/static/css/main.f36eb11f.chunk.css"
+  },
+  {
+    "revision": "5fa0921c557f536eb9fc",
+    "url": "/cat/static/js/2.a839423b.chunk.js"
+  },
+  {
+    "revision": "594d1db979852292f46e",
+    "url": "/cat/static/js/main.bb774fb7.chunk.js"
+  },
+  {
+    "revision": "35eca749081f38f6a793",
+    "url": "/cat/static/js/runtime~main.5add7111.js"
+  }
+]);

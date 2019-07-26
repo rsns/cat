8bfd4719b73f9cbe6f632102d7fc861b2d105a53
arstkvs <arstkvs@gmail.com>
2019-07-26 12:45:25 +0300

    Updates

--- precache-manifest.136c70099915ea67a5501260dab4561b.js
+++ precache-manifest.136c70099915ea67a5501260dab4561b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8ad84e573298d3b764707637bd2f487",
+    "revision": "a74c93075b5d58540e11d8726102bb8c",
     "url": "/cat/index.html"
   },
   {
-    "revision": "594d1db979852292f46e",
+    "revision": "3f2946558117bfc355ef",
     "url": "/cat/static/css/main.f36eb11f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cat/static/js/2.a839423b.chunk.js"
   },
   {
-    "revision": "594d1db979852292f46e",
-    "url": "/cat/static/js/main.bb774fb7.chunk.js"
+    "revision": "3f2946558117bfc355ef",
+    "url": "/cat/static/js/main.a51afd55.chunk.js"
   },
   {
     "revision": "35eca749081f38f6a793",
